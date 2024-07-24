b6732772b6c321f66d6eba932caf76bea178a8c0
boki <lsb530@naver.com>
2024-07-25 02:32:11 +0900

    :tada: init project

---
+++ src/main/kotlin/boki/bokiportfolio/BokiPortfolioApplication.kt
@@ -0,0 +1,11 @@
+package boki.bokiportfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BokiPortfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<BokiPortfolioApplication>(*args)
+}
