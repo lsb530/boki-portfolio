ca973967faa520874cd37d5487a163a9a57d0c5b
boki <lsb530@naver.com>
2024-07-28 21:21:09 +0900

    :wrench: change default profile to dev (#1)
    divide each properties(yaml) file

--- src/main/kotlin/boki/bokiportfolio/BokiPortfolioApplication.kt
+++ src/main/kotlin/boki/bokiportfolio/BokiPortfolioApplication.kt
@@ -1,10 +1,22 @@
 package boki.bokiportfolio
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class BokiPortfolioApplication
+class BokiPortfolioApplication {
+
+    @Value("\${spring.profiles.active}")
+    private val activeProfile: String? = null
+
+    @Bean
+    fun printActiveProfile() = CommandLineRunner {
+        println("Active Profile: $activeProfile")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<BokiPortfolioApplication>(*args)
