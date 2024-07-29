45c37b3c6a01a841d47ad19498b7322dc470a2a8
boki <lsb530@naver.com>
2024-07-30 01:32:22 +0900

    :adhesive_bandage: change repo spring bean (#17)

--- src/main/kotlin/boki/bokiportfolio/repository/UserCrudRepository.kt
+++ src/main/kotlin/boki/bokiportfolio/repository/UserCrudRepository.kt
@@ -2,5 +2,7 @@ package boki.bokiportfolio.repository
 
 import boki.bokiportfolio.entity.User
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserCrudRepository: CrudRepository<User, Long>
