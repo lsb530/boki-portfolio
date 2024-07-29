d07bd0abe55d2eb2a24bca4f3b8ec95db94969be
boki <lsb530@naver.com>
2024-07-29 10:32:59 +0900

    :wrench: add Security Config and log level (#1)

---
+++ src/main/kotlin/boki/bokiportfolio/config/SecurityConfig.kt
@@ -0,0 +1,50 @@
+package boki.bokiportfolio.config
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@ConditionalOnProperty(name = ["server.port"], havingValue = "8080")
+class SecurityConfig {
+
+    private val allowedUrls = arrayOf(
+        "/", "/api", "/api/swagger-ui/**",
+        "/api/docs:**/**",
+//        "/management/**",
+        "/login",
+        "/api/login",
+    )
+
+    @Bean
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.requestMatchers(*allowedUrls).permitAll()
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    .requestMatchers("/api/**").authenticated()
+//                    .requestMatchers("/management/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+            }
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder()
+}
---
+++ src/main/kotlin/boki/bokiportfolio/controller/TestController.kt
@@ -0,0 +1,14 @@
+package boki.bokiportfolio.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping
+    fun test(): ResponseEntity<Any> {
+        return ResponseEntity.ok("ready boki")
+    }
+}
