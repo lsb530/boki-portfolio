4cf873e2dba49e964170bef85843f89a25625d1b
boki <lsb530@naver.com>
2024-07-30 11:50:22 +0900

    :adhesive_bandage: disable default test (#16)

--- src/test/kotlin/boki/bokiportfolio/BokiPortfolioApplicationTests.kt
+++ src/test/kotlin/boki/bokiportfolio/BokiPortfolioApplicationTests.kt
@@ -1,11 +1,13 @@
 package boki.bokiportfolio
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BokiPortfolioApplicationTests {
 
+    @Disabled
     @Test
     fun contextLoads() {
     }
