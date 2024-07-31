61ef0038aa85e332059273861b60921c70273b14
boki <lsb530@naver.com>
2024-08-01 05:56:30 +0900

    :fire: remove useless code (#16)

--- src/main/kotlin/boki/bokiportfolio/security/CustomUserDetails.kt
+++ src/main/kotlin/boki/bokiportfolio/security/CustomUserDetails.kt
@@ -4,11 +4,8 @@ import boki.bokiportfolio.entity.User
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
-import java.util.*
 
 class CustomUserDetails(private val user: User): UserDetails {
-
-    fun getUserId(): Long = user.id!!
 
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
         val roleName = user.role.roleName
