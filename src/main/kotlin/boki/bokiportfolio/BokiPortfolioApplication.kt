124c1c90ef1e707a09ab622c0d09e3b4bc1f9295
boki <lsb530@naver.com>
2024-07-31 20:48:01 +0900

    :wrench: detach jpa audit configuration from spring application (#1)

--- src/main/kotlin/boki/bokiportfolio/BokiPortfolioApplication.kt
+++ src/main/kotlin/boki/bokiportfolio/BokiPortfolioApplication.kt
@@ -6,9 +6,7 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@EnableJpaAuditing
 @SpringBootApplication
 class BokiPortfolioApplication {
 
---
+++ src/main/kotlin/boki/bokiportfolio/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package boki.bokiportfolio.config
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+class JpaConfig {
+
+}
